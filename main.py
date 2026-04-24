87be0d12b68a59110f9395abec6497a2fa259103
Boti21 <botibencsik@gmail.com>
2026-04-24 11:04:45 +0200

    ass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import parser
 import func
+from Node import Switch, EndDevice
 
 
 
@@ -11,7 +12,42 @@ if __name__ == "__main__":
 
     print(test_case)
 
+    MAX_SIMULATION_TIME_US = 1_000_000.0 # 1 second in microseconds
     global_time = 0.0 # us
+
+    # List to make them iterable
+    nodes = []
+    for node in test_case.topology.switches:
+        nodes.append(Switch(id=node.id, domain=node.domain, ports=node.ports))
+    for node in test_case.topology.end_systems:
+        nodes.append(EndDevice(id=node.id, domain=node.domain))
+
+
+
+
+    streams = []
+
+    for stream_id, stream in test_case.streams.items():
+        print(f"Stream {stream_id}: {stream}")
+        # instantiate the sream objects
+
+    links = []
+
+
+
+
+
+
+
+    while global_time < MAX_SIMULATION_TIME_US:
+
+
+
+
+
+
+        global_time += 1 # Advance by 1 us
+        pass
 
 
     pass
