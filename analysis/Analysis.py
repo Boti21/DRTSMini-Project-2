d1b5c0815c6aed15181d0c76a8fce7d7856afcbe
Balage <basza22@student.sdu.dk>
2026-05-08 09:20:54 +0200

    Adapting analysis to codebase

--- analysis/Analysis.py
+++ analysis/Analysis.py
@@ -11,6 +11,7 @@ from CBSPort import *
 from TSNStream import *
 from Node import *
 from Link import *
+from parser import *
 
 class Analyzer:
     def __init__(self, route: list[Node], stream: TSNStream):
@@ -20,12 +21,15 @@ class Analyzer:
     def wcrt_cal(self, route: list[Node], stream: TSNStream):
         for node in route:
             for _, port in node.ports.items():
+                print(f"port.link = {port.link}")
+            for _, port in node.ports.items():
                 if port.port_id == 0: # Only receiving
                     continue
                 self.wcrt += self.spi_calc(port, stream) + \
                              self.hpi_calc(port, stream) + \
                              self.lpi_calc(port, stream) + \
-                             port.link.delay # göh?
+                             port.link.delay * stream.size_bytes/port.link.bandwidth_mbps
+                             # port.link.delay # göh?
         return self.wcrt
 
     def spi_calc(self, port: TSNEgressPort, stream: TSNStream): # Same priority interference
